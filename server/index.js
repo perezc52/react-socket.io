1960907eb2c313f4df4f481cd853814227228de1
Carlos Perez <pcarlos132@yahoo.com>
2023-09-03 17:28:05 -0500

    connected server and client

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const http = require('http')
 const { Server } = require('socket.io')
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
 const server = http.createServer(app)
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+// })
 
 const io = new Server(server, {
     cors: {
